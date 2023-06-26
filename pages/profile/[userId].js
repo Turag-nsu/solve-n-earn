6304d4cdf42650dd9e1cd77ac970c577cad0f552
Turag-nsu <80814798+Turag-nsu@users.noreply.github.com>
2023-06-27 01:45:10 +0600

    Update [userId].js

--- pages/profile/[userId].js
+++ pages/profile/[userId].js
@@ -2,20 +2,17 @@ import { useRouter } from 'next/router';
 import useSWR, { mutate } from 'swr';
 import Profile from '../../components/Profile';
 import { Typography } from '@mui/material';
-// import { useSession, getSession } from 'next-auth/react';
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
 const UserProfilePage = ({ initialData }) => {
   const router = useRouter();
   const { userId } = router.query;
-  // const { userData, problemsData } = initialData;
-  // const { data: session } = useSession();
   const { data: userData, error: userError } = useSWR(`/api/user/${userId}`, fetcher, {
     initialData: initialData.user,
   });
-  const xuser = userData?.user;
-  // console.log(user)
+  const user = userData?.user;
+
   const { data: problems, error: problemsError } = useSWR(`/api/problem`, fetcher, {
     initialData: initialData.problems,
   });
@@ -31,58 +28,57 @@ const UserProfilePage = ({ initialData }) => {
     }
   };
   
+  if (userError || problemsError) {
+    return <Typography>Error fetching user data. Please try again later or contact our developer team.</Typography>;
+  }
 
-    if (userError || problemsError) {
-      return <Typography>Error fetching user data try again later or contact our developer team</Typography>;
-    }
+  return (
+    <Profile
+      name={user?.name}
+      email={user?.email}
+      respectPoints={user?.respectPoints}
+      problems={userProblems}
+      onDeleteProblem={handleProblemDelete}
+    />
+  );
+};
 
-    return (
-      <Profile
-        name={xuser?.name}
-        email={xuser?.email}
-        respectPoints={xuser?.respectPoints}
-        problems={userProblems}
-        onDeleteProblem={handleProblemDelete}
-      />
-    );
+export async function getStaticPaths() {
+  const usersResponse = await fetch('https://solve-n-earn.vercel.app/api/user');
+  const usersData = await usersResponse.json();
+
+  const paths = usersData?.map((user) => ({
+    params: { userId: user.id.toString() },
+  }));
+
+  return {
+    paths,
+    fallback: true,
   };
+}
 
-  export async function getStaticPaths() {
-    const usersResponse = await fetch('https://solve-n-earn.vercel.app/api/user');
-    const usersData = await usersResponse.json();
+export async function getStaticProps(context) {
+  const { userId } = context.params;
+  const userResponse = await fetch(`https://solve-n-earn.vercel.app/api/user/${userId}`);
+  const problemsResponse = await fetch(`https://solve-n-earn.vercel.app/api/problem`);
 
-    const paths = usersData?.map((user) => ({
-      params: { userId: user.id.toString() },
-    }));
-    // console.log(paths)
+  if (!userResponse.ok || !problemsResponse.ok) {
     return {
-      paths,
-      fallback: true,
+      notFound: true,
     };
   }
 
-  export async function getStaticProps(context) {
-    const { userId } = context.params;
-    const userResponse = await fetch(`https://solve-n-earn.vercel.app/api/user/${userId}`);
-    const problemsResponse = await fetch(`https://solve-n-earn.vercel.app/api/problem`);
+  const userData = await userResponse.json();
+  const problemsData = await problemsResponse.json();
 
-    if (!userResponse.ok || !problemsResponse.ok) {
-      return {
-        notFound: true,
-      };
-    }
-
-    const userData = await userResponse.json();
-    const problemsData = await problemsResponse.json();
-    console.log(userData)
-    return {
-      props: {
-        initialData: {
-          user: userData,      // Update property name to 'user'
-          problems: problemsData,
-        },
+  return {
+    props: {
+      initialData: {
+        user: userData,
+        problems: problemsData,
       },
-    };    
-  }
+    },
+  };
+}
 
-  export default UserProfilePage;
+export default UserProfilePage;
